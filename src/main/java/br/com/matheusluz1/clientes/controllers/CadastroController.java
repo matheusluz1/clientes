d31f8106d474a9b3fb1f28d92d0fabcf10e18d2d
Matheus Luz <matheus-sluz@hotmail.com>
2021-12-27 20:16:54 -0300

    teste2

--- src/main/java/br/com/matheusluz1/clientes/controllers/CadastroController.java
+++ src/main/java/br/com/matheusluz1/clientes/controllers/CadastroController.java
@@ -3,13 +3,12 @@ package br.com.matheusluz1.clientes.controllers;
 import br.com.matheusluz1.clientes.entities.Cadastro;
 import br.com.matheusluz1.clientes.services.CadastroService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/cadastros")
@@ -23,10 +22,34 @@ public class CadastroController {
     public ResponseEntity<Cadastro> salvarCadastro(@RequestBody @Valid Cadastro cadastro){
         Cadastro retorno = service.salvarCadastro(cadastro);
         return ResponseEntity.ok(retorno);
-
-
-        }
     }
+
+    @GetMapping("/{idCadastro}")
+    public ResponseEntity<Object> buscarCadastroPorId(@PathVariable Long idCadastro){
+        Optional<Cadastro> cadastro = service.buscarCadastroPorId(idCadastro);
+        if (!cadastro.isPresent())
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cadastro nao encontrado");
+        return ResponseEntity.status(HttpStatus.OK).body(cadastro.get());
+    }
+
+    @DeleteMapping("/{idCadastro}")
+    public ResponseEntity<Object> deletarCadastro(@PathVariable Long idCadastro){
+        Optional<Cadastro> cadastro = service.buscarCadastroPorId(idCadastro);
+        if (!cadastro.isPresent())
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cadastro nao encontrado");
+        service.deletarCadastro(cadastro.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Cadastro deletado com sucesso!");
+    }
+
+    @PutMapping("/{idCadastro}")
+    public ResponseEntity<Object> salvarCadastro( @PathVariable Long idCadastro, @RequestBody @Valid Cadastro cadastroAEditar){
+        Optional<Cadastro> cadastroBase = service.buscarCadastroPorId(idCadastro);
+        if (!cadastroBase.isPresent())
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cadastro nao encontrado");
+        Cadastro retorno = service.editarCadastros(cadastroBase.get(), cadastroAEditar);
+        return ResponseEntity.ok(retorno);
+    }
+}
 
 
 
--- src/test/java/br/com/matheusluz1/clientes/RepositoryTests.java
+++ src/test/java/br/com/matheusluz1/clientes/RepositoryTests.java
@@ -8,18 +8,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ContextConfiguration;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
 @ContextConfiguration
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @Rollback(value = false )
-@WebMvcTest(CadastroController.class)
 public class RepositoryTests {
 
     @Autowired
