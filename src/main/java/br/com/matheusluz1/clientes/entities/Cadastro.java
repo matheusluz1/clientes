b78a98e2ab5686c04f28eb24da80a61fdff72ef5
Matheus Luz <matheus-sluz@hotmail.com>
2021-12-27 20:20:24 -0300

    Revert "teste2"
    This reverts commit d31f8106d474a9b3fb1f28d92d0fabcf10e18d2d.

--- src/main/java/br/com/matheusluz1/clientes/entities/Cadastro.java
+++ src/main/java/br/com/matheusluz1/clientes/entities/Cadastro.java
@@ -1,46 +1,24 @@
 package br.com.matheusluz1.clientes.entities;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.validator.constraints.br.CPF;
 
 import javax.persistence.*;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-import java.time.LocalDate;
 
-
+//(name = ) desnecessário table com o mesmo nome da entity não precisa
 @Entity
 @Table(name = "cadastro")
 @Getter
 @Setter
 public class Cadastro {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotEmpty
+
     @Column(name = "nome", nullable = false)
     private String nome;
 
     @NotNull
-    @Email(message = "email invalido!")
     private String email;
-
-
-    @CPF(message = "CPF invalido!")
-    @Column(name = "cpf")
-    private String cpf;
-
-
-    @Column(name = "data_nascimento")
-    private LocalDate dataNascimento;
-
-
-    @Column(name = "data_cadastro")
-    private LocalDate dataCadastro;
-
-    @Column(name = "data_edicao")
-    private LocalDate dataEdicao;
 }
--- src/main/java/br/com/matheusluz1/clientes/services/CadastroService.java
+++ src/main/java/br/com/matheusluz1/clientes/services/CadastroService.java
@@ -2,12 +2,8 @@ package br.com.matheusluz1.clientes.services;
 
 import br.com.matheusluz1.clientes.entities.Cadastro;
 import br.com.matheusluz1.clientes.repositories.CadastroRepository;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDate;
-import java.util.Optional;
 
 @Service
 public class CadastroService {
@@ -17,25 +13,7 @@ public class CadastroService {
 
 
     public Cadastro salvarCadastro(Cadastro cadastro) {
-        cadastro.setDataCadastro(LocalDate.now());
         return repository.save(cadastro);
-    }
-
-    public Optional<Cadastro> buscarCadastroPorId(Long idCadastro){
-        return repository.findById(idCadastro);
-    }
-
-    public void deletarCadastro(Cadastro cadastro){
-        repository.delete(cadastro);
-    }
-
-    public Cadastro editarCadastros(Cadastro cadastroBase, Cadastro cadastroAEditar) {
-        cadastroBase.setNome(cadastroAEditar.getNome());
-        cadastroBase.setEmail(cadastroAEditar.getEmail());
-        cadastroBase.setDataNascimento(cadastroAEditar.getDataNascimento());
-        cadastroBase.setCpf(cadastroAEditar.getCpf());
-        cadastroBase.setDataEdicao(LocalDate.now());
-        return repository.save(cadastroBase);
     }
 }
 
